e3301c97904b68014480b1f9731de14ce92b4ba6
KamleshPatel98 <kamleshp52170@gmail.com>
2026-04-02 22:10:40 +0530

    subscription table and model create

---
+++ app/Models/Subscription.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Subscription extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'plan_id',
+        'amount',
+        'start_date',
+        'end_date',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('User');
+    }
+
+    public function plan()
+    {
+        return $this->belongsTo('Plan');
+    }
+}
