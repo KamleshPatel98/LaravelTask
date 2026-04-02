ec624824db67296959360f6b2a0ab6e0568b8e15
KamleshPatel98 <kamleshp52170@gmail.com>
2026-04-02 23:28:01 +0530

    subscription expire by scheduler

---
+++ app/Console/Commands/ExpireSubscriptions.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Subscription;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+
+class ExpireSubscriptions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'subscriptions:expire';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Expire subscriptions whose end date has passed';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $expired = Subscription::where('end_date', '<', date('Y-m-d'))
+            ->where('status', 'Active')
+            ->update([
+                'status' => 'Inactive'
+            ]);
+        Cache::forget('user_subscriptions');
+        $this->info("Expired Subscriptions Updated: " . $expired);
+    }
+}
